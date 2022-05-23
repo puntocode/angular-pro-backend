70fb1506b03add527d0b8fbe7e90fe7dc60b2c17
Federico Lopez <federico.lopez@mtel.local>
2022-05-23 09:41:38 -0400

    instalacion express

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
+require('dotenv').config();
+
+// Crear el servidor/aplicación de express
+const app = express();
+
+//Base de datos
+dbConnection();
+
+
+// Directorio Público
+//app.use( express.static('public') );
+
+// CORS
+app.use( cors() );
+
+// Lectura y parseo del body
+//app.use( express.json() );
+
+
+// Rutas
+app.get('/', (req, res) => {
+    console.log('index');
+});
+//app.use( '/api/auth', require('./routes/auth') );
+
+
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+});
